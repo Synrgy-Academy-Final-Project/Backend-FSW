b2f00e80159919a4f50c848fff38a8d1ce6846c6
Misbahudin <misbahudin.id@gmail.com>
2024-01-11 19:40:53 +0700

    fix: unbound method

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from 'express'
 
 export class HomeController {
-    index(_: Request, res: Response): void {
+    public index = (_: Request, res: Response): void => {
         res.render('home')
     }
 }
