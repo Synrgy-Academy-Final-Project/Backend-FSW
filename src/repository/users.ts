0edfa6e0a899c40246b16940f68fb15f91c286cb
Misbahudin <misbahudin.id@gmail.com>
2024-01-12 00:08:29 +0700

    feat: inner join

--- src/repository/users.ts
+++ src/repository/users.ts
@@ -1,12 +1,11 @@
 import { UsersModel } from '../models/users'
-import type { Users } from '../models/users'
 
 export class UserRepository {
-    public saveUser = async (user: Partial<Users>): Promise<UsersModel> => {
-        return await UsersModel.query().insert(user).returning(['email', 'name', 'role'])
-    }
-
     public findUserByEmail = async (email: string): Promise<UsersModel[]> => {
-        return await UsersModel.query().where('email', email).throwIfNotFound()
+        return await UsersModel.query()
+            .where('email', email)
+            .throwIfNotFound()
+            .select('users.*', 'roles.name as role_name')
+            .innerJoin('roles', 'roles.id', 'users.role_id')
     }
 }
