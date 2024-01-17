132f500cca9ee3d458984ce3f50e3a400b01dce5
Misbahudin <misbahudin.id@gmail.com>
2024-01-18 01:35:35 +0700

    chore: update row

--- src/seeds/roles.ts
+++ src/seeds/roles.ts
@@ -2,8 +2,8 @@ import type { Knex } from 'knex'
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex('roles').del()
+    // await knex('roles').del()
 
     // Inserts seed entries
-    await knex('roles').insert([{ name: 'admin' }])
+    await knex('roles').insert([{ name: 'ROLE_ADMIN' }, { name: 'ROLE_USER' }])
 }
