f382ffe41a1c14c01bab255554a124d828fc633c
Misbahudin <misbahudin.id@gmail.com>
2024-01-18 19:33:06 +0700

    feat: add route for trx failed and refund

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -170,6 +170,112 @@ router.get('/api/v1/users/profile', authToken, userController.profile)
  */
 router.get('/api/v1/summary/trx/success', authToken, paymentController.getTransactionSuccess)
 
+/**
+ * @openapi
+ * /api/v1/summary/trx/failed:
+ *  get:
+ *    summary: Get transaction failed
+ *    description: Get summary transaction failed
+ *    tags:
+ *      - Summary
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      202:
+ *        description: Accepted
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                  type: object
+ *                  properties:
+ *                      month:
+ *                          type: string
+ *                      status_code:
+ *                          type: number
+ *                      transaction_status:
+ *                          type: string
+ *                      transaction_count:
+ *                          type: string
+ *                      transaction_amount:
+ *                          type: number
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ *      500:
+ *        description: Internal Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Internal Server Error
+ */
+router.get('/api/v1/summary/trx/failed', authToken, paymentController.getTransactionFailed)
+
+/**
+ * @openapi
+ * /api/v1/summary/trx/refund:
+ *  get:
+ *    summary: Get transaction refund
+ *    description: Get summary transaction refund
+ *    tags:
+ *      - Summary
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      201:
+ *        description: Created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                  type: object
+ *                  properties:
+ *                      month:
+ *                          type: string
+ *                      status_code:
+ *                          type: number
+ *                      transaction_status:
+ *                          type: string
+ *                      transaction_count:
+ *                          type: string
+ *                      transaction_amount:
+ *                          type: number
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Invalid Token
+ *      500:
+ *        description: Internal Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: Internal Server Error
+ */
+router.get('/api/v1/summary/trx/refund', authToken, paymentController.getTransactionRefund)
+
 router.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 export default router
