0a1aae9851ff5856219d2fe25dbdb3c07b2911c7
Misbahudin <misbahudin.id@gmail.com>
2024-01-18 23:50:29 +0700

    add user detail dharmawan

--- src/seeds/usersDetails.ts
+++ src/seeds/usersDetails.ts
@@ -7,12 +7,12 @@ export async function seed(knex: Knex): Promise<void> {
     // Inserts seed entries
     await knex('users_details').insert([
         {
-            id: '9048ecee-962f-4d22-93e4-e3a482cbe773',
+            id: 'd481d809-71a6-465c-b657-e1c7d804964e',
             address: 'Jl. Tegal Raya',
             gender: 'male',
-            phone_number: '+62 812 8655 8895',
-            first_name: 'Misbah',
-            last_name: 'Udin',
+            phone_number: '+62 812 1698 5724',
+            first_name: 'Firdaus',
+            last_name: 'Dharmawan',
             created_date: new Date(),
             updated_date: new Date(),
         },
