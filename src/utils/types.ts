33f3949f050d6ad94f1f5291815b9976a1ec20ed
Misbahudin <misbahudin.id@gmail.com>
2024-01-22 18:25:56 +0700

    feat: add types baseprice airport

---
+++ src/utils/types.ts
@@ -0,0 +1,11 @@
+export interface BasePriceAirportsType {
+    id: string
+    fromCity: string
+    fromCode: string
+    toCity: string
+    toCode: string
+    duration: number
+    price: number
+    createdDate: Date
+    updatedDate: Date
+}
