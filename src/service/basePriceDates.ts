34506365fa8fa00066f4e87233027938db74960f
Misbahudin <misbahudin.id@gmail.com>
2024-01-22 15:51:13 +0700

    feat: add service base price dates

---
+++ src/service/basePriceDates.ts
@@ -0,0 +1,43 @@
+import type { BasePriceDatesModel } from '../models/basePriceDates'
+import { BasePriceDatesRepository } from '../repository/basePriceDates'
+
+export class BasePriceDateService {
+    readonly basePriceDateRepository: BasePriceDatesRepository
+
+    public constructor() {
+        this.basePriceDateRepository = new BasePriceDatesRepository()
+    }
+
+    public saveBasePriceDate = async (
+        id: string,
+        dateOfDeparture: Date,
+        dayCategory: string,
+        price: number,
+        createdDate: Date,
+        updatedDate: Date
+    ): Promise<BasePriceDatesModel> => {
+        return await this.basePriceDateRepository.save(
+            id,
+            dateOfDeparture,
+            dayCategory,
+            price,
+            createdDate,
+            updatedDate
+        )
+    }
+
+    public getAllBasePriceDate = async (): Promise<BasePriceDatesModel[]> => {
+        return await this.basePriceDateRepository.findAll()
+    }
+
+    public updateBasePriceDate = async (
+        id: string,
+        dates: Partial<BasePriceDatesModel>
+    ): Promise<BasePriceDatesModel[]> => {
+        return await this.basePriceDateRepository.updateById(id, dates)
+    }
+
+    public deleteBasePriceDate = async (id: string): Promise<number> => {
+        return await this.basePriceDateRepository.deleteById(id)
+    }
+}
