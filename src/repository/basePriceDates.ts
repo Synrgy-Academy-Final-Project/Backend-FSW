16199e388b4e43ac888d7977998ebea1b354cebc
Misbahudin <misbahudin.id@gmail.com>
2024-01-22 15:51:07 +0700

    feat: add repository base price dates

---
+++ src/repository/basePriceDates.ts
@@ -0,0 +1,43 @@
+import { BasePriceDatesModel } from '../models/basePriceDates'
+import type { BasePriceDates } from '../models/basePriceDates'
+
+export class BasePriceDatesRepository {
+    public save = async (
+        uuid: string,
+        dateOfDeparture: Date,
+        dayCategory: string,
+        price: number,
+        createdDate: Date,
+        updatedDate: Date
+    ): Promise<BasePriceDatesModel> => {
+        return await BasePriceDatesModel.query()
+            .insert({
+                id: uuid,
+                date_from: dateOfDeparture,
+                type: dayCategory,
+                date_price: price,
+                created_date: createdDate,
+                updated_date: updatedDate,
+            })
+            .throwIfNotFound()
+    }
+
+    public findAll = async (): Promise<BasePriceDatesModel[]> => {
+        return await BasePriceDatesModel.query().select(
+            'id',
+            'date_from as dateOfDeparture',
+            'type as dayCategory',
+            'date_price as price',
+            'created_date as createdDate',
+            'updated_date as updatedDate'
+        )
+    }
+
+    public updateById = async (id: string, dates: Partial<BasePriceDates>): Promise<BasePriceDatesModel[]> => {
+        return await BasePriceDatesModel.query().patch(dates).where({ id }).throwIfNotFound().returning('*')
+    }
+
+    public deleteById = async (id: string): Promise<number> => {
+        return await BasePriceDatesModel.query().deleteById(id).throwIfNotFound()
+    }
+}
