aa619fc2518f0c864867c7d910097288d06807e2
Misbahudin <misbahudin.id@gmail.com>
2024-02-05 01:28:19 +0700

    test: add more test case login

--- src/tests/auth/login.test.ts
+++ src/tests/auth/login.test.ts
@@ -3,15 +3,33 @@ import App from '../../app'
 import request from 'supertest'
 
 describe('POST /api/v1/auth/login', () => {
-    const app = new App().app
+  const app = new App().app
 
-    it('responds with json', async () => {
-        const response = await request(app).post('/api/v1/auth/login').send({
-            email: 'mizz@gmail.com',
-            password: 'mizz',
-        })
+  it('should be log in', async () => {
+    const response = await request(app).post('/api/v1/auth/login').send({
+      email: 'mizz@gmail.com',
+      password: 'mizz',
+    })
 
-        expect(response.headers['content-type']).toMatch(/json/)
-        expect(response.status).toBe(200)
-    }, 20000)
-})
+    expect(response.body.token).toEqual(expect.any(String))
+    expect(response.status).toBe(200)
+  })
+
+  it('should be wrong password', async () => {
+    const response = await request(app).post('/api/v1/auth/login').send({
+      email: 'mizz@gmail.com',
+      password: 'wrong password',
+    })
+
+    expect(response.status).toBe(404)
+  })
+
+  it('should be forbidden', async () => {
+    const response = await request(app).post('/api/v1/auth/login').send({
+      email: 'member@gmail.com',
+      password: 'member',
+    })
+
+    expect(response.status).toBe(403)
+  })
+}, 15000)
