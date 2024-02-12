36178f5e5f67c204c7e9b1239948d1297610dac4
Misbahudin <misbahudin.id@gmail.com>
2024-02-12 17:19:31 +0700

    chore: update type in params

--- src/repository/basePriceAirports.ts
+++ src/repository/basePriceAirports.ts
@@ -1,63 +1,62 @@
-import type { UUID } from 'crypto'
 import type { BasePriceAirports } from '../models/basePriceAirports'
 import { BasePriceAirportsModel } from '../models/basePriceAirports'
 
 export class BasePriceAirportsRepository {
-    public save = async (
-        uuid: string,
-        fromAirportId: UUID,
-        toAirportId: UUID,
-        departureCode: string,
-        arrivalCode: string,
-        duration: number,
-        price: number,
-        createdDate: Date,
-        updatedDate: Date
-    ): Promise<BasePriceAirportsModel> => {
-        return await BasePriceAirportsModel.query()
-            .insert({
-                id: uuid,
-                from_airport_id: fromAirportId,
-                to_airport_id: toAirportId,
-                departure_code: departureCode,
-                arrival_code: arrivalCode,
-                duration,
-                airport_price: price,
-                created_date: createdDate,
-                updated_date: updatedDate,
-            })
-            .throwIfNotFound()
-    }
+  public save = async (
+    uuid: string,
+    fromAirportId: string,
+    toAirportId: string,
+    departureCode: string,
+    arrivalCode: string,
+    duration: number,
+    price: number,
+    createdDate: Date,
+    updatedDate: Date
+  ): Promise<BasePriceAirportsModel> => {
+    return await BasePriceAirportsModel.query()
+      .insert({
+        id: uuid,
+        from_airport_id: fromAirportId,
+        to_airport_id: toAirportId,
+        departure_code: departureCode,
+        arrival_code: arrivalCode,
+        duration,
+        airport_price: price,
+        created_date: createdDate,
+        updated_date: updatedDate,
+      })
+      .throwIfNotFound()
+  }
 
-    public findAll = async (): Promise<BasePriceAirports[]> => {
-        return await BasePriceAirportsModel.query()
-            .select(
-                'bpa.id',
-                'apfrom.city as fromCity',
-                'apfrom.code as fromCode',
-                'apto.city as toCity',
-                'apto.code as toCode',
-                'bpa.duration',
-                'bpa.airport_price as price',
-                'bpa.from_airport_id as fromAirportId',
-                'bpa.to_airport_id as toAirportId',
-                'bpa.created_date as createdDate',
-                'bpa.updated_date as updatedDate'
-            )
-            .join('airports as apfrom', 'apfrom.id', 'bpa.from_airport_id')
-            .join('airports as apto', 'apto.id', 'bpa.to_airport_id')
-            .from('baseprice_airports as bpa')
-    }
+  public findAll = async (): Promise<BasePriceAirports[]> => {
+    return await BasePriceAirportsModel.query()
+      .select(
+        'bpa.id',
+        'apfrom.city as fromCity',
+        'apfrom.code as fromCode',
+        'apto.city as toCity',
+        'apto.code as toCode',
+        'bpa.duration',
+        'bpa.airport_price as price',
+        'bpa.from_airport_id as fromAirportId',
+        'bpa.to_airport_id as toAirportId',
+        'bpa.created_date as createdDate',
+        'bpa.updated_date as updatedDate'
+      )
+      .join('airports as apfrom', 'apfrom.id', 'bpa.from_airport_id')
+      .join('airports as apto', 'apto.id', 'bpa.to_airport_id')
+      .from('baseprice_airports as bpa')
+  }
 
-    public findById = async (id: string): Promise<BasePriceAirportsModel> => {
-        return await BasePriceAirportsModel.query().findById(id).throwIfNotFound()
-    }
+  public findById = async (id: string): Promise<BasePriceAirportsModel> => {
+    return await BasePriceAirportsModel.query().findById(id).throwIfNotFound()
+  }
 
-    public updateById = async (id: string, airport: Partial<BasePriceAirports>): Promise<BasePriceAirportsModel[]> => {
-        return await BasePriceAirportsModel.query().patch(airport).where({ id }).throwIfNotFound().returning('*')
-    }
+  public updateById = async (id: string, airport: Partial<BasePriceAirports>): Promise<BasePriceAirportsModel[]> => {
+    return await BasePriceAirportsModel.query().patch(airport).where({ id }).throwIfNotFound().returning('*')
+  }
 
-    public deleteById = async (id: string): Promise<number> => {
-        return await BasePriceAirportsModel.query().deleteById(id).throwIfNotFound()
-    }
+  public deleteById = async (id: string): Promise<number> => {
+    return await BasePriceAirportsModel.query().deleteById(id).throwIfNotFound()
+  }
 }
