947a44e655a1a26b0e49195490d967bd1452677b
Misbahudin <misbahudin.id@gmail.com>
2024-02-15 16:15:37 +0700

    test: articles

---
+++ src/tests/query/articles.test.ts
@@ -0,0 +1,48 @@
+import 'dotenv/config'
+import { expect, test } from 'vitest'
+import knex from 'knex'
+import config from '../../config/knexfile'
+import { Model, raw } from 'objection'
+import { ArticlesModel } from '../../models/articles'
+
+test('get all wisata', async () => {
+  // connect db postgres client
+  Model.knex(knex(config.development))
+
+  const results = await ArticlesModel.query().throwIfNotFound()
+
+  console.log(results)
+  expect(results).toBeTruthy()
+}, 15000)
+
+test('get wisata berdasarkan tempat wisata', async () => {
+  // connect db postgres client
+  Model.knex(knex(config.development))
+
+  const location = 'boro'
+
+  const results = await ArticlesModel.query()
+    .whereRaw(`LOWER(nama_tempat_wisata) LIKE LOWER('%${location}%')`)
+    .throwIfNotFound()
+
+  console.log(results)
+  expect(results).toBeTruthy()
+}, 15000)
+
+test('add jumlah like wisata', async () => {
+  // connect db postgres client
+  Model.knex(knex(config.development))
+
+  const body: number = 100
+
+  const results = await ArticlesModel.query()
+    .findById('29cf3efc-461e-4bbf-a0f3-f2cb62612436')
+
+    .patch({
+      jumlah_like: raw(`jumlah_like + ${body}`),
+    })
+    .throwIfNotFound()
+
+  console.log(results)
+  expect(results).toBeTruthy()
+}, 15000)
