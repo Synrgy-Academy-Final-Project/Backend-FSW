08d3b6a93d2131fb27ae431641b2c26e43cb19cb
Misbahudin <misbahudin.id@gmail.com>
2024-02-17 06:43:17 +0700

    feat: add type on object

--- src/service/payments.ts
+++ src/service/payments.ts
@@ -1,5 +1,6 @@
 import type { PaymentsModel } from '../models/payments'
 import { PaymentRepository } from '../repository/payments'
+import type { TransactionByMonth } from '../utils/types'
 
 export class PaymentService {
   readonly paymentRepository: PaymentRepository
@@ -11,19 +12,16 @@ export class PaymentService {
   public getTransactionPayments = async (): Promise<PaymentsModel[] | unknown> => {
     const results = await this.paymentRepository.findTransactionPayments()
 
-    const transactionsByMonth = {}
+    const transactionsByMonth: TransactionByMonth = {}
 
     for (const result of results) {
       const month = result.month.trim()
-      // @ts-expect-error object airlines is not added type
       if (transactionsByMonth[month] === undefined) {
-        // @ts-expect-error object airlines is not added type
         transactionsByMonth[month] = []
       }
 
-      // @ts-expect-error object airlines is not added type
       transactionsByMonth[month].push({
-        statusCode: result.status_code,
+        statusCode: Number(result.status_code),
         transactionStatus: result.transaction_status,
         transactionCount: Number(result.transaction_count),
         transactionAmount: Number(result.transaction_amount),
