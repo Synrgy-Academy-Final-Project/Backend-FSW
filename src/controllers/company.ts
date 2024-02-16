6112b6b285168a094391b489dfcc1f32ea058198
Misbahudin <misbahudin.id@gmail.com>
2024-02-16 19:18:26 +0700

    feat: add comments

--- src/controllers/company.ts
+++ src/controllers/company.ts
@@ -5,17 +5,22 @@ import type { IReqBody, IReqParams } from '../utils/types'
 import { DataError } from 'objection'
 
 export class CompanyController {
+  // variable hanya bisa di instance oleh company service dan tidak bisa di ubah value nya
   readonly companyService: CompanyService
 
   public constructor() {
+    // instance company service
     this.companyService = new CompanyService()
   }
+
+  // method untuk mendapatkan semua company
 
   public getAllCompany = async (
     _: Request,
     res: Response
   ): Promise<Response<unknown, Record<string, unknown>> | undefined> => {
     try {
+      // mendapatkan semua company
       const companies = await this.companyService.getAllCompany()
 
       return res.status(200).json({
@@ -23,10 +28,13 @@ export class CompanyController {
         message: 'Success',
         data: companies,
       })
+      // jika terjadi error
     } catch (error: any) {
+      // tampilkan error di console
       console.log(error)
-
+      // jika error status 404
       if (error.statusCode === 404) {
+        // kirim response 404 bahwa company tidak ditemukan
         return res.status(404).json({
           status: 404,
           message: 'Company Not Found',
@@ -41,12 +49,16 @@ export class CompanyController {
     }
   }
 
+  // method mendapatkan company berdasarkan id
+
   public getCompanyById = async (
     req: Request<IReqParams>,
     res: Response
   ): Promise<Response<any, Record<string, any>>> => {
     try {
+      // get id params
       const { id } = req.params
+      // get company by id
       const company = await this.companyService.getCompanyById(id)
 
       return res.status(200).json({
@@ -54,17 +66,21 @@ export class CompanyController {
         message: 'Success',
         data: company,
       })
+      // jika terjadi error
     } catch (error: any) {
+      // tampilkan error di console
       console.error(error)
-
+      // jika error dari data error
       if (error instanceof DataError) {
+        // kirim response 400 request nya salah
         return res.status(400).json({
           status: 400,
           message: 'Bad Request',
         })
       }
-
+      // jika eror status 404
       if (error.statusCode === 404) {
+        // kirim status 404 bahwa id company tidak ditemukan
         return res.status(404).json({
           status: 404,
           message: 'ID Company Not Found',
@@ -79,14 +95,18 @@ export class CompanyController {
     }
   }
 
+  // method update company
+
   public updateCompanyById = async (
     req: Request<IReqParams, unknown, IReqBody>,
     res: Response
   ): Promise<Response<any, Record<string, any>>> => {
     try {
+      // get id params
       const { id } = req.params
+      // get url from req body
       const { url } = req.body
-
+      // update company by id
       const company = await this.companyService.updateCompanyById(id, { url })
 
       return res.status(200).json({
@@ -94,17 +114,21 @@ export class CompanyController {
         message: 'Success update company',
         data: company,
       })
+      // jika terjadi errpr
     } catch (error: any) {
+      // tampilkan error di console
       console.error(error)
-
+      // jika error dari data error
       if (error instanceof DataError) {
+        // kirim response 400 requestnya buruk
         return res.status(400).json({
           status: 400,
           message: 'Bad Request',
         })
       }
-
+      // jika error status 404
       if (error.statusCode === 404) {
+        // kirim response 404 bahwa id company tidak ditemukan
         return res.status(404).json({
           status: 404,
           message: 'ID Company Not Found',
