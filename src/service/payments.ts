43df115b4dfb91aa87a5927d74ca27d8151b82c8
Misbahudin <misbahudin.id@gmail.com>
2024-02-17 07:24:15 +0700

    feat: add comment

--- src/service/payments.ts
+++ src/service/payments.ts
@@ -3,23 +3,30 @@ import { PaymentRepository } from '../repository/payments'
 import type { TransactionByMonth } from '../utils/types'
 
 export class PaymentService {
+  // membuat variable readonly dan hanya bisa di instance oleh payment repository
   readonly paymentRepository: PaymentRepository
 
   public constructor() {
     this.paymentRepository = new PaymentRepository()
   }
 
+  // fungsi mendapatkan semua transaksi pembayaran
+
   public getTransactionPayments = async (): Promise<PaymentsModel[] | unknown> => {
+    // mencari transaksi pembayaran
     const results = await this.paymentRepository.findTransactionPayments()
-
+    // membuat objek kosong untuk menampung semua key value yang dibutuhkan oleh client
     const transactionsByMonth: TransactionByMonth = {}
-
+    // loop data
     for (const result of results) {
+      // menghilangkan spasi
       const month = result.month.trim()
+      // jika value dari key month nya kosong
       if (transactionsByMonth[month] === undefined) {
+        // membuat array kosong
         transactionsByMonth[month] = []
       }
-
+      // mengirim data objek dengan key value pada array month
       transactionsByMonth[month].push({
         statusCode: Number(result.status_code),
         transactionStatus: result.transaction_status,
--- src/service/transactions.ts
+++ src/service/transactions.ts
@@ -3,39 +3,47 @@ import { TransactionRepository } from '../repository/transactions'
 import type { TransactionByAirline } from '../utils/types'
 
 export class TransactionService {
+  // membuat variable readonly dan hanya bisa di instance oleh transaction repository
   readonly transactionRepository: TransactionRepository
 
   public constructor() {
     this.transactionRepository = new TransactionRepository()
   }
 
+  // method untuk mendapatkan laporan transaksi
+
   public getReportTransaction = async (): Promise<TransactionsModel[]> => {
     return await this.transactionRepository.findReportTransaction()
   }
 
+  // method untuk mendapatkan maskapai paling banyak di pesan
+
   public getTheMostSoldoutAirlines = async (): Promise<TransactionsModel[] | unknown> => {
     const results = await this.transactionRepository.findTheMostSoldoutAirlines()
-
+    // membuat objek kosong untuk menampung semua key value yang dibutuhkan oleh client
     const airlines: TransactionByAirline = {}
-
+    // loop data
     for (const result of results) {
+      // mendapatkan nama maskapai
       const airlineName = result.airlineName
-
+      // jika key pada objek airlines undefined
       if (airlines[airlineName] === undefined) {
+        // membuat objek baru pada key airlineName
         airlines[airlineName] = {
           airlineName,
           totalSoldout: 0,
           airplanes: [],
         }
       }
-
+      // menjumlahkan total maskapai yang di pesan
       airlines[airlineName].totalSoldout += Number(result.totalSoldoutAirline)
+      // mengirim data objek dengan key value pada array airplanes
       airlines[airlineName].airplanes.push({
         airplaneName: result.airplaneName,
         totalSoldout: Number(result.totalSoldoutAirplane),
       })
     }
-
+    // mendapatkan value dari object airlines
     const finalResults = Object.values(airlines)
 
     return finalResults
